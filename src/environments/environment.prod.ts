eccfad040a3a532c22db4befa9c9a4eef9a18ed4
rostia-vrt <vrostyk07@gmail.com>
2020-02-20 17:43:46 +0200

    problem solving

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDo9q7Pg2ji7WUyLtxxvlyn8QET4SeMxew',
+    authDomain: 'cars-from-sweden.firebaseapp.com',
+    databaseURL: 'https://cars-from-sweden.firebaseio.com',
+    projectId: 'cars-from-sweden',
+    storageBucket: 'cars-from-sweden.appspot.com',
+    messagingSenderId: '1098202757346',
+    appId: '1:1098202757346:web:5e1130316e6e82fa10c6bb'
+    }
 };
